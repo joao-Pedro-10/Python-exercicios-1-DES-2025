c442e2a7246804d3509e98f0441089a77b7658ef
joao-Pedro-10 <joao.grande.padilha@escola.pr.gov.br>
2025-05-29 12:20:09 +0000

    exercicio01

--- exercicio01.py
+++ exercicio01.py
@@ -2,5 +2,14 @@
 # Ele quer saber qual curso teve mais avaliações no último mês.
 
 # Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
-# Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
-
+# Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve
+curso01 = int(input("digite a quantidade de acessos do curso01, "))
+curso02 = int(input("digite a quantidade de acessos do curso02, "))
+ 
+if curso01 > curso02 :
+    print("curso01 teve mais acesso. ")
+elif curso02 > curso01 :
+    print("curso02 teve mais acessos. ")
+else:
+    print("os dois tiveram mesmo numeros de acesso. ")
+   
