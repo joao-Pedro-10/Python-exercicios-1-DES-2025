bc7c95aa9e0967f1b3407b88913a6f088cca592e
joao-Pedro-10 <joao.grande.padilha@escola.pr.gov.br>
2025-06-03 13:43:28 +0000

    terminei exercicio-02

--- exercicio02.py
+++ exercicio02.py
@@ -3,7 +3,12 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
-terefaX = (int(input("digite o tempo em que foi feito a tarefaX:"))
-tarefaY = (int(input("digite o tempo em que foi feito a tarefaX:"))
-tarefaZ = (int(input("digite o tempo em que foi feito a tarefaX:"))
-if trabalhoX <
+dia01 = int(input("digite o tempo em que foi feito a tarefaX:"))
+dia02 = int(input("digite o tempo em que foi feito a tarefaY:"))
+dia03 = int(input("digite o tempo em que foi feito a tarefaZ:"))
+
+if dia01 < 0 or dia02 < 0 or dia03 < 0:
+    print("erro negativo.") 
+else:
+    soma = dia01 + dia02 + dia03
+    print(f"tempo total da atividade: {soma} dias ")
--- exercicio07.py
+++ exercicio07.py
@@ -4,5 +4,12 @@
 # Aprovado (>=7)
 # Em treinamento (>=5 e <7)
 # Reprovado (<5)
+media1 = float(input("digite a primeira medida: "))
+media2 = float(input("digite a segunda medida: "))
+media3 = float(input("digite a terceira medida: "))
 
+soma = media1 + media2 + media3 
+
+media = soma / 3
+if =
 
