bcd50e5a9066f9c66b4a6703e579205afe5ab276
joao-Pedro-10 <joao.grande.padilha@escola.pr.gov.br>
2025-06-10 11:53:28 +0000

    exercicio06.py_finalizado

--- exercicio06.py
+++ exercicio06.py
@@ -2,9 +2,9 @@
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
 
 plataforme = int(input("digite que horas voce quer acessar a plataforma: "))
-if plataforma < 9:
+if plataforme < 9:
     print("acesso negado. ")
-elif plataforma > 21:
+elif plataforme > 21:
     print("acesso negado.")
 else:
     print("acesso permitido. ")
